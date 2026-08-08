7df1bf64177a4725dbc43a47095a7652196b310a
Francesco Paraggio <francesco.paraggio@ext.euipo.europa.eu>
2026-08-08 15:49:11 +0200

    Email di contatto: primainumeri@proton.me

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,2 +1,2 @@
-// Recapito pubblico del gruppo promotore. Da aggiornare con l'indirizzo reale.
-export const CONTACT_EMAIL = 'info@primailuoghi.it';
+// Recapito pubblico del gruppo promotore.
+export const CONTACT_EMAIL = 'primainumeri@proton.me';
